78e6eef123310642f8402b60b677e1a2df449485
Sander Devisscher <sander.devisscher@inbo.be>
2025-10-16 14:07:51 +0200

    improve cut_into_fragments
    #5

--- src/cut_into_fragments.R
+++ src/cut_into_fragments.R
@@ -1,18 +1,23 @@
-cut_into_fragments <- function(fragment_length = 60){
+cut_into_fragments <- function(input_dir = NULL,
+                               fragment_length = 60){
   
+  browser()
   # Set input_dir
-  input_dir <- choose.dir(caption = "Set input_dir")
+  if(is.null(input_dir) || is.na(input_dir)){
+    input_dir <- rstudioapi::selectDirectory()
+  }
   
   if(!dir.exists(input_dir)){
     stop(paste(input_dir, "doesn't exist => choose another folder"))
   }
+  cat("reading from:", input_dir,"\n")
   
   # Set Cores
   num_cores <- parallel::detectCores()
-  print(num_cores)
+  cat("Number of cores advailable:", num_cores, "\n")
   
   if(num_cores - 4 > 1){
-    if(askYesNo(msg = "Use parallel computing?")){
+    if(askYesNo(msg = "Use parallel computing? \n")){
       cores_to_use <- num_cores - 4
     }else{
       cores_to_use <- 1
@@ -25,7 +30,7 @@ cut_into_fragments <- function(fragment_length = 60){
   
   # Install warbleR
   if(!"warbleR" %in% installed.packages()){
-    cat("warbleR is not installed => installing")
+    cat("warbleR is not installed => installing \n")
     remotes::install_github("maRce10/warbleR")
   }
   
